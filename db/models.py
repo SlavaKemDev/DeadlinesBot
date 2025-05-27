ed421251c8701a20a49200d11f3b489fe173fd07
Вячеслав Чертан <slavakemdev@gmail.com>
2025-05-27 13:35:12 +0300

    Add groups and subscriptions to database

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, BigInteger, ForeignKey, Integer
 from .session import Base
-from typing import Optional
+from typing import Optional, List
 
 
 class User(Base):
@@ -11,3 +11,43 @@ class User(Base):
     first_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[str] = mapped_column(String(50))
     username: Mapped[Optional[str]] = mapped_column(String(50), nullable=True)
+
+    subscriptions: Mapped[List["UserSubscription"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+    options: Mapped[List["GroupOption"]] = relationship(
+        back_populates="group", cascade="all, delete-orphan"
+    )
+
+
+class GroupOption(Base):
+    __tablename__ = "group_options"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    group_id: Mapped[int] = mapped_column(Integer, ForeignKey("groups.id"))
+    name: Mapped[str] = mapped_column(String(50))
+    telegram_channel_id: Mapped[Optional[int]] = mapped_column(BigInteger, nullable=True)
+
+    group: Mapped["Group"] = relationship(back_populates="options")
+    subscribers: Mapped[List["UserSubscription"]] = relationship(
+        back_populates="group_option", cascade="all, delete-orphan"
+    )
+
+
+class UserSubscription(Base):
+    __tablename__ = "user_subscriptions"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    group_option_id: Mapped[int] = mapped_column(Integer, ForeignKey("group_options.id"))
+
+    user: Mapped["User"] = relationship(back_populates="subscriptions")
+    group_option: Mapped["GroupOption"] = relationship(back_populates="subscribers")
