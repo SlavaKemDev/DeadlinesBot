4cda399d0a4a1f3b66892ad06898d2f77377570e
Вячеслав Чертан <slavakemdev@gmail.com>
2025-05-27 13:17:07 +0300

    Add LLM model support for extract deadlines from messages

---
+++ ai/model.py
@@ -0,0 +1,66 @@
+import json
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+from typing import List
+
+from gigachat import GigaChat
+from gigachat.models import Chat, Messages, MessagesRole
+
+
+load_dotenv()
+
+
+class Model:
+    @staticmethod
+    def _generate_prompt(message_history: List[str], current_deadlines: list[tuple[str, datetime]]):
+        text_current_deadlines = []
+        for i in range(len(current_deadlines)):
+            text_current_deadlines.append((current_deadlines[i][0], current_deadlines[i][1].strftime("%d.%m.%Y %H:%M:%S")))
+
+        message_text = "\n\n--- СЛЕДУЮЩЕЕ СООБЩЕНИЕ ---\n\n".join(message_history)
+
+        current_dir = os.path.dirname(__file__)
+        prompt_path = os.path.join(current_dir, "prompt.txt")
+        with open(prompt_path, "r") as f:
+            prompt = f.read().format(
+                message_text=message_text,
+                current_deadlines=json.dumps(text_current_deadlines, ensure_ascii=False),
+                current_date=datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
+                year=datetime.now().year
+            )
+
+        return prompt
+
+    def __init__(self, **kwargs):
+        kwargs['credentials'] = kwargs.get('credentials', os.environ['GIGACHAT_TOKEN'])
+        kwargs['scope'] = kwargs.get('scope', os.environ['GIGACHAT_SCOPE'])
+        kwargs['verify_ssl_certs'] = kwargs.get('verify_ssl_certs', False)
+        kwargs['model'] = kwargs.get('model', 'GigaChat-2')  # GigaChat Lite 2 работает сильно лучше первой версии
+
+        self.model = GigaChat(**kwargs)
+
+    async def get_response(self, message_history: List[str], current_deadlines: list[tuple[str, datetime]]):
+        PAYLOAD = Chat(
+            messages=[
+                Messages(
+                    role=MessagesRole.USER,
+                    content=Model._generate_prompt(message_history, current_deadlines),
+                ),
+            ],
+            update_interval=0.1,
+        )
+
+        answer = ""
+
+        async for chunk in self.model.astream(PAYLOAD):
+            answer += chunk.choices[0].delta.content
+
+        print(answer)
+
+        new_deadlines = json.loads(answer.replace('(', '[').replace(')', ']'))
+
+        for i in range(len(new_deadlines)):
+            new_deadlines[i] = (new_deadlines[i][0], datetime.strptime(new_deadlines[i][1], "%d.%m.%Y %H:%M:%S"))
+
+        return new_deadlines
---
+++ ai/test_model.py
@@ -0,0 +1,41 @@
+import unittest
+from datetime import datetime, timedelta
+import json
+import os
+
+from .model import Model
+
+
+class TestModel(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        current_dir = os.path.dirname(__file__)
+        texts_path = os.path.join(current_dir, "tests.json")
+
+        with open(texts_path) as f:
+            self.data = json.load(f)
+
+        self.model = Model()
+
+    async def test_simple(self):
+        response = await self.model.get_response(self.data['simple'], [])
+
+        self.assertEqual(1, len(response))
+        self.assertTrue("ИДЗ" in response[0][0])
+        self.assertEqual(datetime.strptime("24.03.2025 23:00:00", "%d.%m.%Y %H:%M:%S"), response[0][1])
+
+    async def test_reply(self):
+        response = await self.model.get_response(self.data['reply'], [("ИДЗ 4", datetime.strptime("21.05.2025 23:00:00", "%d.%m.%Y %H:%M:%S"))])
+
+        self.assertEqual(1, len(response))
+        self.assertTrue("ИДЗ" in response[0][0])
+        self.assertEqual(datetime.strptime("25.05.2025 23:00:00", "%d.%m.%Y %H:%M:%S"), response[0][1])
+
+    async def test_complex_date(self):
+        response = await self.model.get_response(self.data['complex_date'], [])
+
+        self.assertEqual(1, len(response))
+        self.assertEqual(datetime.strptime("20.04.2025 04:00:00", "%d.%m.%Y %H:%M:%S"), response[0][1])
+
+
+if __name__ == '__main__':
+    unittest.main()
